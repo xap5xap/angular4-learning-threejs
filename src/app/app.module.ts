87789cd5bb084fa1888b379bb07ed5eb3a0a3839
Xavier Perez <xap5xap@gmail.com>
2017-04-23 12:24:55 -0500

    Add side navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { Chapter1Component } from './chapter1/chapter1.component';
+
+const appRoutes: Routes = [
+  { path: 'chapter1', component: Chapter1Component },
+  { path: '',
+    redirectTo: '/chapter1',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Chapter1Component
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
